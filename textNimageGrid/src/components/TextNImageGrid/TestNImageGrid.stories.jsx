985ce86965a0c1727e1fa02022e0ee1e330b2992
Suvel <suvelrathneswar08@gmail.com>
2021-10-16 19:09:37 +0530

    added Storybook

---
+++ textNimageGrid/src/components/TextNImageGrid/TestNImageGrid.stories.jsx
@@ -0,0 +1,33 @@
+import TextNImageGrid from "./index";
+
+export default {
+  title: "Components/TextNImageGrid",
+  component: TextNImageGrid,
+  // decorators: [
+  //   (Story) => (
+  //     <div style={{ height: "700px", width: "500px" }}>
+  //       <Story />
+  //     </div>
+  //   ),
+  // ],
+};
+
+const Template = (args) => <TextNImageGrid {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  title: "Text and Image module 2",
+  subtitle: `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum condimentum eros ex, quis cursus arcu tempor quis. Ut elementum pellentesque orci at rhoncus. Donec in pellentesque turpis. Suspendisse imperdiet viverra quam in fringilla. In ullamcorper maximus rhoncus. Proin vitae maximus neque. Vivamus imperdiet sem vitae ullamcorper suscipit. `,
+  imageOne:
+    "https://images.unsplash.com/photo-1503602642458-232111445657?ixid:MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib:rb-1.2.1&auto:format&fit:crop&w:687&q:80",
+  imageOneAlt: "image of laptop",
+  imageOneSubText: "Image taken from a free resource",
+  imageTwo:
+    "https://images.unsplash.com/photo-1503602642458-232111445657?ixid:MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib:rb-1.2.1&auto:format&fit:crop&w:687&q:80",
+  imageTwoAlt: "image of laptop",
+  imageTwoSubText: "Image taken from a free resource",
+  imageThree:
+    "https://images.unsplash.com/photo-1503602642458-232111445657?ixid:MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib:rb-1.2.1&auto:format&fit:crop&w:687&q:80",
+  imageThreeAlt: "image of laptop",
+  imageThreeSubText: "Image taken from a free resource",
+};
--- textNimageGrid/src/components/TextNImageGrid/index.jsx
+++ textNimageGrid/src/components/TextNImageGrid/index.jsx
@@ -6,33 +6,37 @@ import ImageThree from "./ImageThree";
 
 import "./index.css";
 
-const TextNImageGrid = () => {
+const TextNImageGrid = ({
+  title,
+  subtitle,
+  imageOne,
+  imageOneAlt,
+  imageOneSubText,
+  imageTwo,
+  imageTwoAlt,
+  imageTwoSubText,
+  imageThree,
+  imageThreeAlt,
+  imageThreeSubText,
+  // reversed,
+}) => {
   return (
     <div className="txtnimg-grid">
-      <TextContent
-        title={"Text and Image module 2"}
-        subtitle={`Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum condimentum eros ex, quis cursus arcu tempor quis. Ut elementum pellentesque orci at rhoncus. Donec in pellentesque turpis. Suspendisse imperdiet viverra quam in fringilla. In ullamcorper maximus rhoncus. Proin vitae maximus neque. Vivamus imperdiet sem vitae ullamcorper suscipit. `}
-      />
+      <TextContent title={title} subtitle={subtitle} />
       <ImageOne
-        altTxt={"image of laptop"}
-        image={
-          "https://images.unsplash.com/photo-1503602642458-232111445657?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=687&q=80"
-        }
-        imageSubText={"Image taken from a free resource"}
+        altTxt={imageOneAlt}
+        image={imageOne}
+        imageSubText={imageOneSubText}
       />
       <ImageTwo
-        altTxt={"image of laptop"}
-        image={
-          "https://images.unsplash.com/photo-1605098801888-def2d3c129e4?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=627&q=80"
-        }
-        imageSubText={"Image taken from a free resource"}
+        altTxt={imageTwoAlt}
+        image={imageTwo}
+        imageSubText={imageTwoSubText}
       />
       <ImageThree
-        altTxt={"image of laptop"}
-        image={
-          "https://images.unsplash.com/photo-1519947486511-46149fa0a254?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=687&q=80"
-        }
-        imageSubText={"Image taken from a free resource"}
+        altTxt={imageThreeAlt}
+        image={imageThree}
+        imageSubText={imageThreeSubText}
       />
     </div>
   );
